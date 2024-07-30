d14b2c9caceddc9c8b9c97c998fb306a57c539c7
Andrew Fenton <andrewfenton898@gmail.com>
2024-07-29 22:40:15 -0400

    added backend

--- extension/script.js
+++ extension/script.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 async function fetchData() {
     // const res = await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
     // const record = await res.json();
     document.getElementById("text-input").placeholder = "Enter GPT question";
     document.getElementById("text-output").value = "";
-
+    
     
 }
 fetchData();
