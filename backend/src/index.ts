68b96b196c4f9d40602beddd637dd216bc461973
Andrew Fenton <andrewfenton898@gmail.com>
2024-08-18 23:52:29 -0400

    started login implementation (need to fix)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,41 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { gptRouter } from "./routes/gptRouter";
+import session from "express-session";
+import passport from "passport";
+import sequelize from "./config/sequelize";
+import gptRouter from "./routes/gpt";
+import authRouter from "./routes/auth"
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+// Session configuration
+app.use(session({
+  secret: process.env.SESSION_SECRET || "",
+  resave: false,
+  saveUninitialized: true,
+}));
+
+// Middleware
 app.use(cors());
 app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+app.use("/auth", authRouter);
+app.use('/gpt', gptRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Backend API");
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
-
-app.use('/gpt', gptRouter);
+// Test the database connection and sync models
+sequelize.sync().then(() => {
+  app.listen(3000, () => {
+    console.log(`Server is running on port ${port}.`);
+  });
+}).catch(error => console.log("Database connection failed:", error));
