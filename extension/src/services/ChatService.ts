e861c75276c5c6c2ffdd74ce3667e7bf4155524b
Andrew Fenton <andrewfenton898@gmail.com>
2024-09-08 00:51:59 -0400

    fixed gpt endpoint bug

--- extension/src/services/ChatService.ts
+++ extension/src/services/ChatService.ts
@@ -4,7 +4,17 @@ import { Message } from "../types/types";
 class ChatService {
     async sendMessages(messages: Message[]): Promise<string> {
         try {
-            const response = await axios.post("http://localhost:3000/gpt/query", JSON.stringify(messages));
+            console.log("ChatService received:", messages);
+            console.log("Stringified messages:", JSON.stringify(messages));
+            const response = await axios.post(
+                "http://localhost:3000/gpt/query",
+                messages,
+                {
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }
+            );
             return response.data;
         } catch (e) {
             console.error(e);
